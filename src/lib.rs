c774122ca04740475aac070f1316469690d7f294
Samuel Åkesson <sermuns@lysator.liu.se>
2026-07-18 15:56:53 +0200

    feat: use saturating sub, adjust comments

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 use image::{Pixel, Rgb, RgbImage};
 
-/// Compute vertical gradient
+/// Compute vertical gradient (above minus below)
 pub fn compute_gradient_y_of_image(img: &RgbImage, grad_x: &mut RgbImage) {
     for (x, y, Rgb([r, g, b])) in grad_x
         .enumerate_pixels_mut()
@@ -11,13 +11,13 @@ pub fn compute_gradient_y_of_image(img: &RgbImage, grad_x: &mut RgbImage) {
         let Rgb([above_r, above_g, above_b]) = img[(x, y - 1)];
         let Rgb([here_r, here_g, here_b]) = img[(x, y)];
 
-        *r = above_r.wrapping_sub(here_r);
-        *g = above_g.wrapping_sub(here_g);
-        *b = above_b.wrapping_sub(here_b);
+        *r = above_r.saturating_sub(here_r);
+        *g = above_g.saturating_sub(here_g);
+        *b = above_b.saturating_sub(here_b);
     }
 }
 
-/// Compute horizontal gradient
+/// Compute horizontal gradient (left minus right)
 pub fn compute_gradient_x_of_image(img: &RgbImage, grad_y: &mut RgbImage) {
     for (x, y, Rgb([r, g, b])) in grad_y.enumerate_pixels_mut() {
         if x == 0 {
@@ -27,12 +27,13 @@ pub fn compute_gradient_x_of_image(img: &RgbImage, grad_y: &mut RgbImage) {
         let Rgb([left_r, left_g, left_b]) = img[(x - 1, y)];
         let Rgb([here_r, here_g, here_b]) = img[(x, y)];
 
-        *r = left_r.wrapping_sub(here_r);
-        *g = left_g.wrapping_sub(here_g);
-        *b = left_b.wrapping_sub(here_b);
+        *r = left_r.saturating_sub(here_r);
+        *g = left_g.saturating_sub(here_g);
+        *b = left_b.saturating_sub(here_b);
     }
 }
 
+/// Sum `grad_x` and `grad_y`
 pub fn compute_gradient_magnitude(grad_x: &RgbImage, grad_y: &RgbImage, out: &mut RgbImage) {
     for ((grad_x_pixel, grad_y_pixel), out_pixel) in
         grad_x.pixels().zip(grad_y.pixels()).zip(out.pixels_mut())
