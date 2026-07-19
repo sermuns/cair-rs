1734e7d562dd434aaf47c4a3929845db368a9629
Samuel Åkesson <sermuns@lysator.liu.se>
2026-07-19 23:32:05 +0200

    feat: fix/improve CLI example

--- examples/seam-removal-cli/src/main.rs
+++ examples/seam-removal-cli/src/main.rs
@@ -1,23 +1,40 @@
-use std::time::Instant;
+use std::{path::PathBuf, time::Instant};
 
-use anyhow::bail;
+use anyhow::ensure;
 use cair::{compute_gradient_magnitude, remove_seams};
+use clap::Parser;
 use image::{DynamicImage, ImageReader, RgbImage};
 
+#[derive(Parser)]
+struct Args {
+    img_path: PathBuf,
+
+    output_path: PathBuf,
+
+    #[arg(short = 'v', long, default_value_t = 0)]
+    vertical_seams_to_remove: usize,
+
+    #[arg(short = 'H', long, default_value_t = 0)]
+    horizontal_seams_to_remove: usize,
+}
+
 fn main() -> anyhow::Result<()> {
-    let Some(img_path) = std::env::args().nth(1) else {
-        bail!("no image path provided!");
-    };
+    let Args {
+        img_path,
+        output_path,
+        vertical_seams_to_remove,
+        horizontal_seams_to_remove,
+    } = Args::parse();
+
+    ensure!(
+        vertical_seams_to_remove > 0 || horizontal_seams_to_remove > 0,
+        "You haven't requested to remove any seams!"
+    );
 
     let img = ImageReader::open(img_path)?.decode()?.into_rgb8();
     let (width, height) = img.dimensions();
 
     let before = Instant::now();
-
-    // let mut grad_x = RgbImage::new(width, height);
-    // compute_gradient_x_of_image(&img, &mut grad_x);
-    // let mut grad_y = RgbImage::new(width, height);
-    // compute_gradient_y_of_image(&img, &mut grad_y);
 
     let mut grad_magnitude = RgbImage::new(width, height);
     compute_gradient_magnitude(&img, &mut grad_magnitude);
@@ -28,11 +45,16 @@ fn main() -> anyhow::Result<()> {
 
     let energy = DynamicImage::ImageRgb8(grad_magnitude).into_luma8();
 
-    let carved = remove_seams(&img, &energy, 200);
+    let carved = remove_seams(
+        &img,
+        &energy,
+        horizontal_seams_to_remove,
+        vertical_seams_to_remove,
+    );
 
     println!("removed seams in {:?}", before.elapsed());
 
-    carved.save("out.png")?;
+    carved.save(output_path)?;
 
     Ok(())
 }
